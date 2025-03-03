372bf2a40f03fd946718749746d3737f08615fac
MaEsTr0oF <skhvalchev@mail.ru>
2025-03-03 16:04:37 +0300

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,7 @@
-/// <reference types="node" />
-declare const __dirname: string;
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import * as path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  publicDir: 'public',
-  build: {
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-      },
-      output: {
-        assetFileNames: (assetInfo: { name?: string }) => {
-          if (!assetInfo.name) return 'assets/[name]-[hash][extname]';
-          let extType = assetInfo.name.split('.')[1]
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            extType = 'img'
-          }
-          return `assets/${extType}/[name]-[hash][extname]`;
-        },
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js',
-      },
-    },
-  },
 })
